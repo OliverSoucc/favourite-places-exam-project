e3bbf1a02b662c75ecb05838c4ad9ad63d67065b
Jan Nahalka <janx803y@easv365.dk>
2023-06-08 11:20:05 +0200

    config to run ios

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Great Places',
       theme: theme,
-      home: SignUp(),
+      home: const SignUp(),
     );
   }
 }
